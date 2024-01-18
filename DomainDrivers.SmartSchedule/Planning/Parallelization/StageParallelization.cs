3267df19a48d63150b537422e5b6ea6c28a16094
iszreter <ignacyszreter@gmail.com>
2024-01-18 20:11:20 +0100

    graph topological sort to parallelize stages

--- DomainDrivers.SmartSchedule/Planning/Parallelization/StageParallelization.cs
+++ DomainDrivers.SmartSchedule/Planning/Parallelization/StageParallelization.cs
@@ -4,6 +4,31 @@ public class StageParallelization
 {
     public ParallelStagesList Of(ISet<Stage> stages)
     {
-        return ParallelStagesList.Empty();
+        return CreateSortedNodesRecursively(stages, ParallelStagesList.Empty());
+    }
+    
+    private ParallelStagesList CreateSortedNodesRecursively(ISet<Stage> remainingNodes, ParallelStagesList accumulatedSortedNodes)
+    {
+        var alreadyProcessedNodes = accumulatedSortedNodes.All
+            .SelectMany(n => n.Stages)
+            .ToList();
+        var nodesWithoutDependencies = WithAllDependenciesPresentIn(remainingNodes, alreadyProcessedNodes);
+
+        if (nodesWithoutDependencies.Count == 0)
+        {
+            return accumulatedSortedNodes;
+        }
+
+        var newSortedNodes = accumulatedSortedNodes.Add(new ParallelStages(nodesWithoutDependencies));
+        var newRemainingNodes = new HashSet<Stage>(remainingNodes);
+        newRemainingNodes.ExceptWith(nodesWithoutDependencies);
+        return CreateSortedNodesRecursively(newRemainingNodes, newSortedNodes);
+    }
+
+    private ISet<Stage> WithAllDependenciesPresentIn(ISet<Stage> toCheck, IEnumerable<Stage> presentIn)
+    {
+        return toCheck
+            .Where(n => n.Dependencies.All(d => presentIn.Contains(d)))
+            .ToHashSet();
     }
 }
