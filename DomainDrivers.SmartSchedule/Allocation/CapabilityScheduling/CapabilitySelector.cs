de62c3aac1a1bea727c3c96889c5d6b144f1b300
Ignacy S <42671368+ignacyszreter@users.noreply.github.com>
2024-02-24 12:38:50 +0100

    implemented capability selector

--- DomainDrivers.SmartSchedule/Allocation/CapabilityScheduling/CapabilitySelector.cs
+++ DomainDrivers.SmartSchedule/Allocation/CapabilityScheduling/CapabilitySelector.cs
@@ -2,25 +2,50 @@
 
 namespace DomainDrivers.SmartSchedule.Allocation.CapabilityScheduling;
 
-public class CapabilitySelector
+public record CapabilitySelector(ISet<Capability> Capabilities, SelectingPolicy SelectingPolicy)
 {
-    public static CapabilitySelector CanPerformOneOf(ISet<Capability> capabilities)
+    public static CapabilitySelector CanPerformAllAtTheTime(ISet<Capability> capabilities)
     {
-        return null!;
+        return new CapabilitySelector(capabilities, SelectingPolicy.AllSimultaneously);
     }
 
-    public static CapabilitySelector CanPerformAllAtTheTime(ISet<Capability> beingAnAdmin)
+    public static CapabilitySelector CanPerformOneOf(ISet<Capability> capabilities)
     {
-        return null!;
+        return new CapabilitySelector(capabilities, SelectingPolicy.OneOfAll);
     }
 
     public bool CanPerform(Capability capability)
     {
-        return false;
+        return Capabilities.Contains(capability);
     }
 
     public bool CanPerform(ISet<Capability> capabilities)
     {
-        return false;
+        if (capabilities.Count == 1)
+        {
+            return new HashSet<Capability>(Capabilities).IsSupersetOf(capabilities);
+        }
+
+        return SelectingPolicy == SelectingPolicy.AllSimultaneously &&
+               new HashSet<Capability>(Capabilities).IsSupersetOf(capabilities);
     }
+
+    public virtual bool Equals(CapabilitySelector? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Capabilities.SetEquals(other.Capabilities)
+               && SelectingPolicy == other.SelectingPolicy;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Capabilities.CalculateHashCode(), SelectingPolicy);
+    }
+}
+
+public enum SelectingPolicy
+{
+    AllSimultaneously,
+    OneOfAll
 }
