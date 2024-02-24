0761d6e93385cc069a67754d6cc684857d938535
Grzegorz Orwat <39306499+grzegorzorwat@users.noreply.github.com>
2024-02-24 12:29:34 +0100

    introduced capability selector (just tests)

---
+++ DomainDrivers.SmartSchedule.Tests/Allocation/CapabilityScheduling/CapabilitySelectorTest.cs
@@ -0,0 +1,40 @@
+﻿using DomainDrivers.SmartSchedule.Allocation.CapabilityScheduling;
+using DomainDrivers.SmartSchedule.Shared;
+
+namespace DomainDrivers.SmartSchedule.Tests.Allocation.CapabilityScheduling;
+
+public class CapabilitySelectorTest
+{
+    static readonly Capability Rust = new Capability("RUST", "SKILL");
+    static readonly Capability BeingAnAdmin = new Capability("ADMIN", "PERMISSION");
+    static readonly Capability Java = new Capability("JAVA", "SKILL");
+
+    [Fact]
+    public void AllocatableResourceCanPerformOnlyOneOfPresentCapabilities()
+    {
+        //given
+        var adminOrRust = CapabilitySelector.CanPerformOneOf(new HashSet<Capability>() { BeingAnAdmin, Rust });
+
+        //expect
+        Assert.True(adminOrRust.CanPerform(BeingAnAdmin));
+        Assert.True(adminOrRust.CanPerform(Rust));
+        Assert.False(adminOrRust.CanPerform(new HashSet<Capability>() { Rust, BeingAnAdmin }));
+        Assert.False(adminOrRust.CanPerform(new Capability("JAVA", "SKILL")));
+        Assert.False(adminOrRust.CanPerform(new Capability("LAWYER", "PERMISSION")));
+    }
+
+    [Fact]
+    public void AllocatableResourceCanPerformSimultaneousCapabilities()
+    {
+        //given
+        var adminAndRust = CapabilitySelector.CanPerformAllAtTheTime(new HashSet<Capability>() { BeingAnAdmin, Rust });
+
+        //expect
+        Assert.True(adminAndRust.CanPerform(BeingAnAdmin));
+        Assert.True(adminAndRust.CanPerform(Rust));
+        Assert.True(adminAndRust.CanPerform(new HashSet<Capability>() { Rust, BeingAnAdmin }));
+        Assert.False(adminAndRust.CanPerform(new HashSet<Capability>() { Rust, BeingAnAdmin, Java }));
+        Assert.False(adminAndRust.CanPerform(Java));
+        Assert.False(adminAndRust.CanPerform(new Capability("LAWYER", "PERMISSION")));
+    }
+}
---
+++ DomainDrivers.SmartSchedule/Allocation/CapabilityScheduling/CapabilitySelector.cs
@@ -0,0 +1,26 @@
+﻿using DomainDrivers.SmartSchedule.Shared;
+
+namespace DomainDrivers.SmartSchedule.Allocation.CapabilityScheduling;
+
+public class CapabilitySelector
+{
+    public static CapabilitySelector CanPerformOneOf(ISet<Capability> capabilities)
+    {
+        return null!;
+    }
+
+    public static CapabilitySelector CanPerformAllAtTheTime(ISet<Capability> beingAnAdmin)
+    {
+        return null!;
+    }
+
+    public bool CanPerform(Capability capability)
+    {
+        return false;
+    }
+
+    public bool CanPerform(ISet<Capability> capabilities)
+    {
+        return false;
+    }
+}
