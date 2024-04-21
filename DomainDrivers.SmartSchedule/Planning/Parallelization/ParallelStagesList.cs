d4f294988a0aed8709bfa016a443943fabcf607b
iszreter <ignacyszreter@gmail.com>
2024-04-21 22:24:20 +0200

    task to implement taking into account shared resource while parallelization

--- DomainDrivers.SmartSchedule/Planning/Parallelization/ParallelStagesList.cs
+++ DomainDrivers.SmartSchedule/Planning/Parallelization/ParallelStagesList.cs
@@ -4,20 +4,9 @@ namespace DomainDrivers.SmartSchedule.Planning.Parallelization;
 
 public record ParallelStagesList(IList<ParallelStages> All)
 {
-    public static ParallelStagesList Empty()
-    {
-        return new ParallelStagesList(new List<ParallelStages>());
-    }
-
     public string Print()
     {
         return string.Join(" | ", All.Select(parallelStages => parallelStages.Print()));
-    }
-
-    public ParallelStagesList Add(ParallelStages newParallelStages)
-    {
-        var result = new List<ParallelStages>(All) { newParallelStages };
-        return new ParallelStagesList(result);
     }
 
     public virtual bool Equals(ParallelStagesList? other)
