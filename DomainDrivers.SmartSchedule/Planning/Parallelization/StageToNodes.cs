9f04681ee49dc7b349fb37709cc0671d70d399c9
iszreter <ignacyszreter@gmail.com>
2024-04-21 22:25:33 +0200

    implemented task to implement taking into account shared resource while parallelization

--- DomainDrivers.SmartSchedule/Planning/Parallelization/StageToNodes.cs
+++ DomainDrivers.SmartSchedule/Planning/Parallelization/StageToNodes.cs
@@ -12,9 +12,36 @@ public class StagesToNodes
         {
             var stage = stages[i];
             result = ExplicitDependencies(stage, result);
+            result = SharedResources(stage, stages.Skip(i + 1).ToList(), result);
         }
 
         return new Nodes(new HashSet<Node>(result.Values));
+    }
+
+    private IDictionary<string, Node> SharedResources(Stage stage, IList<Stage> with, IDictionary<string, Node> result)
+    {
+        foreach (var other in with)
+        {
+            if (stage.Name != other.Name)
+            {
+                if (stage.Resources.Intersect(other.Resources).Any())
+                {
+                    if (other.Resources.Count > stage.Resources.Count)
+                    {
+                        var node = result[stage.Name];
+                        node = node.DependsOn(result[other.Name]);
+                        result[stage.Name] = node;
+                    }
+                    else
+                    {
+                        var node = result[other.Name];
+                        node = node.DependsOn(result[stage.Name]);
+                        result[other.Name] = node;
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     private IDictionary<string, Node> ExplicitDependencies(Stage stage, IDictionary<string, Node> result)
